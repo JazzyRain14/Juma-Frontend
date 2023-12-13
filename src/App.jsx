36e24a0b8b10e6f5286797e37daf10af9f4f5253
JazzyRain14 <ajadiolamilekan14@gmail.com>
2023-12-13 16:38:04 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>I love Jesus</h1>
+      <h1 className='flex justify-center items-center text-3xl text-[#246bce] font-bold'>I think i love her</h1>
     </>
   )
 }
