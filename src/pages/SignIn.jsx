851f1c0713850a9cbcc7199ffd52777e7ce8f9be
JazzyRain14 <ajadiolamilekan14@gmail.com>
2023-12-26 00:35:33 +0100

    Started form handling and validation

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react'
 import google from '../Images/google-color-icon.png'
 import { FaCartArrowDown, FaEye, FaEyeSlash } from "react-icons/fa";
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { Link } from 'react-router-dom';
+import dining from '../Images/Dining.png'
 
 const SignIn = () => {
     const [type, settype] = useState("password");
     const [eye, seteye] = useState(FaEyeSlash);
-    const [password, setpassword] = useState("");
-
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        onSubmit: (values) => {
+            console.log(values);
+        },
+        validationSchema : yup.object({
+            email: yup.string().required('required')
+        })
+    })
 
     const reveal = (e) => {
         e.preventDefault()
@@ -25,81 +38,108 @@ const SignIn = () => {
 
     return (
         <>
-            <div className='bg-[#CCCCCC] h-100 min-h-screen  grid grid-flow-row gap-8 px-8 pt-4'>
+            <div className='bg-[#CCCCCC] h-100 min-h-screen  grid grid-flow-row gap-8 p-6 md:grid-cols-grid1 md:gap-4 md:p-8'>
+
                 {/* {/* left div */}
+                <div className=' row-span-1'>
+                    {/* Logo */}
+                    <div className='border-r-2 border-projectRed flex items-center mb-8'>
+                        <span className=' text-3xl text-[#1A1110]'>
+                            <i>
+                                <FaCartArrowDown />
+                            </i>
+                        </span>
+                        <span
+                            className='text-4xl ml-1 font-extrabold text-[#FF0000]'
 
-                {/* Logo */}
-                <div className=' row-span-1 border-r-2 border-projectRed flex items-center mb-8'>
-                    <span className=' text-3xl text-[#1A1110]'>
-                        <i>
-                            <FaCartArrowDown />
-                        </i>
-                    </span>
-                    <span
-                        className='text-4xl ml-1 font-extrabold text-[#FF0000]'
+                            style={{
+                                WebkitTextStroke: ".65px #FFFFFF",
+                            }}
+                        >
+                            Juma
+                        </span>
+                    </div>
 
-                        style={{
-                            WebkitTextStroke: ".65px #FFFFFF",
-                        }}
+                    {/* vector */}
+                    <div className='hidden md:flex flex-col gap-[35px] justify-center items-center'
                     >
-                        Juma
-                    </span>
+                        <div
+                            className='flex justify-center h-[350px] 
+                    w-[450px]'>
+                            <img src={dining} alt="" className='h-full w-full object-cover'/>
+                        </div>
+                        <span className=' text-[14px] lg:w-[450px]'>
+                            Welcome To <span className='text-[#FF0000]'>Juma</span> Food and Confectionaries, We satisfy your cravings. Be Sure to check out on our <Link>Terms and Conditions</Link>.
+                        </span>
+                    </div>
                 </div>
 
                 {/* right div */}
                 <div className=' row-span-6'>
-                    <h1 className='text-4xl'>Welcome Back</h1>
-                    <p className=' font-semibold text-[16px]'><span className=' text-xl text-projectRed'>Sign In</span>, Let's satisfy your cravings</p>
-                    <form action="" className='mt-3'>
-                        <div className='mt-2 mb-4'>
-                            <label className=' block mb-2 text-[#424242] text-[26px]'>Email</label>
-                            <div className='border border-projectBorder p-4 rounded bg-[#ffffff]'>
+                    <h1 className='text-4xl md:text-2xl'>Welcome Back</h1>
+                    <p className=' font-semibold text-[16px] md:text-[12px]'>
+                        <span className=' text-xl text-projectRed md:text-lg'>Sign In</span>, Let's satisfy your cravings
+                    </p>
+                    <form
+                        action=""
+                        className='mt-3'
+                        onSubmit={formik.handleSubmit}
+                    >
+                        <div className='mt-2 mb-4 md:mt-1 md:mb-3'>
+                            <label className=' block mb-2 text-[#424242] text-[26px] md:text-[15px]'>Email</label>
+                            <div className='border border-projectBorder p-4 rounded bg-[#ffffff] md:p-2'>
                                 <input
                                     type="mail"
-                                    className=' w-full bg-red-30 text-2xl  outline-none bg-transparent'
+                                    className=' w-full text-2xl  outline-none bg-transparent md:text-sm'
                                     name='email'
-                                    placeholder='info@example.com' />
+                                    placeholder='info@example.com'
+                                onChange={formik.handleChange}/>
                             </div>
+                            <small>{ formik.errors.email}</small>
                         </div>
-                        <div className='mt-2 mb-4'>
-                            <label className=' block mb-2 text-[#424242] text-[26px]'>Password</label>
+                        <div className='mt-2 mb-4 md:mt-1 md:mb-3'>
+                            <label className=' block mb-2 text-[#424242] text-[26px] md:text-[15px]'>Password</label>
 
-                            <div className='border border-projectBorder p-4 rounded flex items-center  bg-[#ffffff]'>
+                            <div className='border border-projectBorder p-4 rounded flex items-center bg-[#ffffff] md:p-2'>
                                 <input
-                                    className=' w-full bg-red-30 text-2xl  outline-none bg-transparent'
+                                    className=' w-full text-2xl  outline-none bg-transparent md:text-sm'
                                     type={type}
                                     name='password'
                                     placeholder='Password'
-                                    onChange={(e) => setpassword(e.target.value)}
+                                    onChange={formik.handleChange}
+
                                 />
                                 {
-                                    password === "" ?
+                                    formik.values.password === "" ?
                                         <span className='ml-7'></span> :
-                                        <button onClick={reveal}
-                                            type='button'
+                                        <i onClick={reveal}
                                             className='
-                                             rounded-[50%] flex items-center justify-center w-[30px] h-[100%] cursor-pointer text-[25px]'>
+                                             rounded-[50%] flex items-center justify-center w-[30px] h-[100%] cursor-pointer text-[25px] md:text-[20px]'>
                                             {eye}
-                                        </button>
+                                        </i>
                                 }
                             </div>
                         </div>
-                        <button className=' w-[100%] p-5 mt-2 mb-4 text-2xl font-semibold rounded bg-[#d61313] hover:bg-[#d61313f3] text-[#ffffff]'>
+                        <button
+                            type='btn'
+                            className=' w-[100%] p-5 mt-2 mb-4 text-2xl font-semibold rounded bg-[#d61313] hover:bg-[#d61313f3] text-[#ffffff]
+                         md:mt-1 md:mb-3 md:p-3 md:text-lg'
+                        >
                             Sign In
                         </button>
-                        <button className=' w-[100%] p-5 mt-2 mb-4 text-2xl font-semibold border border-projectBlack  text-projectBlack rounded flex justify-center items-center hover:bg-[#f8f8f8]'>
+                        <button className=' w-[100%] p-5 mt-2 mb-4 text-2xl font-semibold border border-projectBlack  text-projectBlack rounded flex justify-center items-center hover:bg-[#f8f8f8] md:p-3 md:mb-3 md:mt-1 md:text-lg'>
                             <span>
-                                <img src={google} alt="" className='mr-2 w-7' /></span>
+                                <img src={google} alt="" className='mr-2 w-5 md:w-7' /></span>
                             Continue with Google
                         </button>
                     </form>
-                    <p className=' border text-xl'>Don't have an account? <Link to='/signup' className='text-[#FF0000] font-medium'>Sign Up for free</Link>
+                    <p className=' text-xl md:text-sm'>Don't have an account? <Link to='/signup' className='text-[#FF0000] font-medium'>Sign Up for free</Link>
                     </p>
                 </div>
                 <div>
 
                 </div>
-            </div>
+            </div >
         </>
     )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import google from '../Images/google-color-icon.png'
 import { FaCartArrowDown, FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import dining from '../Images/Dining.png'
 
 const SignUp = () => {
     const [type, settype] = useState("password");
@@ -21,11 +22,14 @@ const SignUp = () => {
         }
     }
 
+
+
     return (
         <>
-            <div className='bg-[#CCCCCC] h-100 min-h-screen grid grid-flow-row gap-8 px-8 py-4 lg:grid-cols-grid1 lg:gap-4 lg:px-4'>
+            <div className='bg-[#CCCCCC] h-100 min-h-screen grid grid-flow-row gap-8 p-6 md:grid-cols-grid1 md:gap-4 md:p-8'>
+
                 {/* {/* left div */}
-                <div className=' row-span-1 border'>
+                <div className=' row-span-1'>
                     {/* Logo */}
                     <div className='border-r-2 border-projectRed flex items-center mb-8'>
                         <span className=' text-3xl text-[#1A1110]'>
@@ -43,15 +47,17 @@ const SignUp = () => {
                             Juma
                         </span>
                     </div>
+
                     {/* vector */}
                     <div
-                        className='hidden sm:flex flex-col gap-[35px] justify-center items-center'
+                        className='hidden md:flex flex-col gap-[35px] justify-center items-center'
                     >
                         <div
-                            className='flex justify-center bg-dining h-[350px] 
+                            className='flex justify-center h-[350px] 
                     w-[450px]'>
+                            <img src={dining} alt="" className='h-full w-full object-cover' />
                         </div>
-                        <span className='w-[450px] text-[14px]'>
+                        <span className='text-[14px]  lg:w-[450px]'>
                             Welcome To <span className='text-[#FF0000]'>Juma</span> Food and Confectionaries, We satisfy your cravings. Be Sure to check out on our <Link>Terms and Conditions</Link>.
                         </span>
                     </div>
@@ -60,7 +66,9 @@ const SignUp = () => {
                 {/* right div */}
                 <div className=' row-span-6'>
                     <h1 className='text-4xl md:text-2xl'>Welcome to <span className=' text-projectRed'>Juma</span></h1>
-                    <p className=' font-semibold text-[16px] md:text-[12px]'><span className=' text-xl text-projectRed md:text-lg'>Sign Up</span>, Let's satisfy your cravings</p>
+                    <p className=' font-semibold text-[16px] md:text-[12px]'>
+                        <span className=' text-xl text-projectRed md:text-lg'>Sign Up</span>, Let's satisfy your cravings
+                    </p>
                     <form action="" className='mt-3'>
 
                         <div className='mt-2 mb-4 md:mt-1 md:mb-3'>
@@ -88,36 +96,12 @@ const SignUp = () => {
                         <div className='mt-2 mb-4 md:mt-1 md:mb-3'>
 
                             <label className=' block mb-2 text-[#424242] text-[26px] md:text-[15px]'>Password</label>
-                            <div className='border border-projectBorder p-4 rounded bg-[#ffffff] md:p-2'>
+                            <div className='border border-projectBorder p-4 rounded  flex items-center bg-[#ffffff] md:p-2'>
                                 <input
-                                    type="text"
+                                    type={type}
                                     className=' w-full text-2xl  outline-none bg-transparent md:text-sm'
                                     name='password'
-                                    placeholder='enter your password' />
-                            </div>
-                        </div>
-
-
-                        {/* <div className='mt-2 mb-4'>
-                            <label className=' block mb-2 text-[#424242] text-[26px]'>Email</label>
-                            <div className='border border-projectBorder p-4 rounded bg-[#ffffff]'>
-                                <input
-                                    type="mail"
-                                    className=' w-full bg-red-30 text-2xl  outline-none bg-transparent'
-                                    name='email'
-                                    placeholder='info@example.com' />
-                            </div>
-                        </div>
-
-                        <div className='mt-2 mb-4'>
-                            <label className=' block mb-2 text-[#424242] text-[26px]'>Password</label>
-
-                            <div className='border border-projectBorder p-4 rounded flex items-center  bg-[#ffffff]'>
-                                <input
-                                    className=' w-full bg-red-30 text-2xl  outline-none bg-transparent'
-                                    type={type}
-                                    name='password'
-                                    placeholder='Password'
+                                    placeholder='enter your password'
                                     onChange={(e) => setpassword(e.target.value)}
                                 />
                                 {
@@ -126,17 +110,18 @@ const SignUp = () => {
                                         <button onClick={reveal}
                                             type='button'
                                             className='
-                                             rounded-[50%] flex items-center justify-center w-[30px] h-[100%] cursor-pointer text-[25px]'>
+                                             rounded-[50%] flex items-center justify-center w-[30px] h-[100%] cursor-pointer text-[25px] md:text-[20px]'>
                                             {eye}
                                         </button>
                                 }
                             </div>
-                        </div> */}
+                        </div>
+
 
 
                         <button className=' w-[100%] p-5 mt-2 mb-4 text-2xl font-semibold rounded bg-[#d61313] hover:bg-[#d61313f3] text-[#ffffff]
                          md:mt-1 md:mb-3 md:p-3 md:text-lg'>
-                            Sign In
+                            Sign Up
                         </button>
                         <button className=' w-[100%] p-5 mt-2 mb-4 text-2xl font-semibold border border-projectBlack  text-projectBlack rounded flex justify-center items-center hover:bg-[#f8f8f8] md:p-3 md:mb-3 md:mt-1 md:text-lg'>
                             <span>
@@ -144,7 +129,7 @@ const SignUp = () => {
                             Continue with Google
                         </button>
                     </form>
-                    <p className='text-xl md:text-sm'>Don't have an account? <Link to='/signin' className='text-[#FF0000] font-medium'>Sign Up for free</Link>
+                    <p className='text-xl md:text-sm'>Already have an account? <Link to='/signin' className='text-[#FF0000] font-medium'>Sign In</Link>
                     </p>
                 </div>
                 <div>
