8b662e0e9bdead76c9670d30a00d6e199a4185ab
Avanin <asamuvictor03@gmail.com>
2024-03-16 22:28:31 +0100

    updated homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Section1 from '../components/User/home/Section1'
+import { FaCartArrowDown} from "react-icons/fa";
 import TopNav from '../components/User/home/TopNav'
 import SideBar from '../components/SideBar'
 import Section2 from '../components/User/home/Section2'
@@ -44,7 +45,20 @@ const HomePage = () => {
     return (
 
         <>
-        { loading === true ? <div className=''>I dey load oooo.....</div>: 
+        { loading === true ? <div className='flex justify-center'>I  <span className=' text-3xl text-[#1A1110] mt-[7px]'>
+                            <i>
+                                <FaCartArrowDown />
+                            </i>
+                        </span>
+                        <span
+                            className='text-4xl ml-1 font-extrabold text-[#FF0000]'
+
+                            style={{
+                                WebkitTextStroke: ".65px #FFFFFF",
+                            }}
+                        >
+                            Juma
+                        </span></div>: 
             <div className='pt-4 px-4'>
                 <TopNav />
                 <hr className='rounded-lg bg-[#585757] h-[2px] my-2' />
